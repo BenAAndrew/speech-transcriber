ec9a2a2c4a62d6a8f08ff40d6314ea6ed432672f
BenAAndrew <benandrew89@gmail.com>
2022-11-06 20:25:14 +0100

    Add punctuator

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import argparse
 import uuid
 import os
+from punctuator import Punctuator
 
 from audio import convert_audio
 from select_transcriber import select_transcriber, Transcriber
 
 
-def transcribe_file(path: str, transcriber_name: str):
-    assert os.path.isfile(path), f"{path} file does not exist"
+def transcribe_file(file_path: str, transcriber_name: str, punctuator_path: str = None):
+    assert os.path.isfile(file_path), f"{file_path} file does not exist"
     converted_path = f"{uuid.uuid4()}.wav"
-    convert_audio(path, converted_path)
+    convert_audio(file_path, converted_path)
     transcriber = select_transcriber(transcriber_name)
-    print(transcriber.transcribe(converted_path))
+    transcription = transcriber.transcribe(converted_path)
+    if punctuator_path:
+        punctuator = Punctuator(punctuator_path)
+        transcription = punctuator.punctuate(transcription)
+    print(transcription)
     os.remove(converted_path)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Transcribe a given audio file")
-    parser.add_argument("-p", "--path", type=str, help="Audio file path", required=True)
+    parser.add_argument("-f", "--file", type=str, help="Audio file path", required=True)
     parser.add_argument("-t", "--transcriber", type=str, help="Transcriber to use", choices=[t.value for t in Transcriber], required=True)
+    parser.add_argument("-p", "--punctuator", type=str, help="Punctuation model to use", required=False)
     args = parser.parse_args()
-    transcribe_file(args.path, args.transcriber)
+    transcribe_file(args.file, args.transcriber, args.punctuator)
 
 
 if __name__ == "__main__":
